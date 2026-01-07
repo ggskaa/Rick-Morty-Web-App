e8c7d641108e1c9823a2d5265778518281331ce2
ggskaa <a.i.rodionov08@gmail.com>
2026-01-07 20:02:44 +0200

    Fix Tailwind v4 config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// vitejs.dev
 export default defineConfig({
   plugins: [
     react(),
